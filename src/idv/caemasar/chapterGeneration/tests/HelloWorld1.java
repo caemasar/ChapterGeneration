cba5051984d16a8e4091149ead8886c4094d95a2
bisc <bisc@minstone.com.cn>
2018-12-18 14:23:50 +0800

    idea分支，提交测试class again

--- src/idv/caemasar/chapterGeneration/tests/HelloWorld1.java
+++ src/idv/caemasar/chapterGeneration/tests/HelloWorld1.java
@@ -11,5 +11,6 @@ package idv.caemasar.chapterGeneration.tests;
 public class HelloWorld1 {
     public static void main(String[] args) {
         System.out.println("HelloWorld1");
+        System.out.println("HelloWorld1,again!");
     }
 }
