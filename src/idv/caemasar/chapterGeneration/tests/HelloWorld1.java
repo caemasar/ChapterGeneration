4f7a6d3667191c375564282c704135dd7cdece7d
bisc <bisc@minstone.com.cn>
2018-12-18 14:11:26 +0800

    idea分支，提交测试class

---
+++ src/idv/caemasar/chapterGeneration/tests/HelloWorld1.java
@@ -0,0 +1,15 @@
+package idv.caemasar.chapterGeneration.tests;
+
+/**
+ * HelloWorld
+ * 
+ * @copyright 广州明动软件有限公司 Copyright (c) 2018
+ * @since 2018-12-18
+ * @author BISC
+ * @version 1.0
+ */
+public class HelloWorld1 {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld1");
+    }
+}
