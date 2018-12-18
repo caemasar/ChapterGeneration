0bf2b4d485fa1a3c59eb0d1c6e69aa42ffa930f6
Caemasar <caemasar@protonmail.com>
2018-12-18 14:39:17 +0800

    eclipse分支提交测试class bye

--- src/idv/caemasar/chapterGeneration/HelloWorld0.java
+++ src/idv/caemasar/chapterGeneration/HelloWorld0.java
@@ -4,5 +4,6 @@ public class HelloWorld0 {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld0");
 		System.out.println("HelloWorld0,again!");
+		System.out.println("Bye,World0~");
 	}
 }
