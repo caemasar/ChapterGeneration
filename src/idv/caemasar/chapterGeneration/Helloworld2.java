0ca9f5edfcef912cab11f6efd7bb643c1fc79ac3
Caemasar <caemasar@protonmail.com>
2018-12-18 17:50:30 +0800

    还可以合并远程的分支，本地的分支和远程的分支其实是对应的，只不过可能本地的分支没有远程的分支那么的全

--- src/idv/caemasar/chapterGeneration/Helloworld2.java
+++ src/idv/caemasar/chapterGeneration/Helloworld2.java
@@ -4,5 +4,6 @@ public class Helloworld2 {
 	public static void main(String[] args) {
 		System.out.println("eeeeeee22222222");
 //		测试可知git合并操作是先跳到某个分支然后把某个分支的合并到跳到的分支上
+//		还可以合并远程的分支，本地的分支和远程的分支其实是对应的，只不过可能本地的分支没有远程的分支那么的全
 	}
 }
