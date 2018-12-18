0f057ed958871a979fdc044bc0d2d5911b89483e
Caemasar <caemasar@protonmail.com>
2018-12-18 17:47:06 +0800

    测试可知git合并操作是先跳到某个分支然后把某个分支的合并到跳到的分支上

--- src/idv/caemasar/chapterGeneration/Helloworld2.java
+++ src/idv/caemasar/chapterGeneration/Helloworld2.java
@@ -3,5 +3,6 @@ package idv.caemasar.chapterGeneration;
 public class Helloworld2 {
 	public static void main(String[] args) {
 		System.out.println("eeeeeee22222222");
+//		测试可知git合并操作是先跳到某个分支然后把某个分支的合并到跳到的分支上
 	}
 }
