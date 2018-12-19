82da31dc7ec135258b67fff22afafd6213a0c1b6
bisc <bisc@minstone.com.cn>
2018-12-19 10:46:26 +0800

    尝试用idea编辑Eclipse分支，同时提交看看分支树的样子

---
+++ src/idv/caemasar/chapterGeneration/tests/HelloWorld3.java
@@ -0,0 +1,16 @@
+package idv.caemasar.chapterGeneration.tests;
+
+/**
+ * HelloWorld3
+ *
+ * @author BISC
+ * @version 1.0
+ * @copyright 广州明动软件有限公司 Copyright (c) 2018
+ * @since 2018-12-19
+ */
+public class HelloWorld3 {
+    public static void main(String[] args) {
+        System.out.println();
+        //尝试用idea编辑Eclipse分支，同时提交看看分支树的样子
+    }
+}
